8bd0c8525ca7d534467728a24a0827db1bb847d8
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-03-03 23:04:33 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :email, presence: true,unique:true
          validates :family_name, presence: true
          validates :first_name, presence: true
          validates :family_name_kana, presence: true
