a500d6f37e1d0720e748e945b90d3a809cbe0746
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-03-07 17:03:56 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,18 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  with_options presence:true do
+    validates :title
+    validates :category_id
+    validates :condition_id
+    validates :price
+    validates :details
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
+
+
   
 end
