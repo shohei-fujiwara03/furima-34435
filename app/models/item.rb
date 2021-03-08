f382f489a1fccfff94a0dc0582120c368ef3c135
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-03-08 17:34:54 +0900

    active_hashの導入、各activehash::Baseの作成、バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,29 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :shipping_date
+
   belongs_to :user
   has_one_attached :image
 
   with_options presence:true do
+
     validates :title
-    validates :category_id
-    validates :condition_id
     validates :price
     validates :details
+
+    with_options numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :condition_id
     validates :shipping_fee_id
     validates :prefecture_id
     validates :shipping_date_id
+    end
+
   end
 
 
