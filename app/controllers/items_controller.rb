06e07a90e3dd7a5dbc329ac5731462ee246fc1fe
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-03-12 10:43:24 +0900

    ルーティング、アクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,7 +33,17 @@ class ItemsController < ApplicationController
     else
       render :edit
      end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
     end
+  end
+  
+  end
 end
 
 
