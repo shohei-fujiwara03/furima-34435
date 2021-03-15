afeea4ac576a4bab3093d96b843dc0c644fae450
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-03-16 00:01:22 +0900

    コントローラーでformオブジェクトのインスタンスを生成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,24 @@
 class OrdersController < ApplicationController
 
   
+
+  def new
+    @order_shipping = OrderShipping.new
+  end
+
+  def create
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      @order_shipping.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping).permit(:zip_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id,item_id:current_item.id)
+  end
 end
