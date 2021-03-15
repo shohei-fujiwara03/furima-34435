330a8343d2c0d5d69d5ec04834d5b51df40d8ef5
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-03-15 18:16:55 +0900

    バリデーション、active_hashの設定

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,3 +1,14 @@
 class Shipping < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
   belongs_to :order
+
+  with_options presence:true  do
+    validates :zip_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :phone_number,format: {with: /\A[0-9]{3}-[0-9]{4}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id,numericality: { other_than: 1 } 
 end
