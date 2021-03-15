85caaff689ac8a13705d2312485b92c1a234cc10
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-03-15 19:06:30 +0900

    order_shippingのバリデーション

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,4 +1,16 @@
 class OrderShipping
   include ActiveModel::Model
   attr_accessor :zip_code,:prefecture_id,:city,:address,:building_name,:phone_number,:order_id,:user_id,:item_id
+
+  with_options presence:true  do
+    validates :city
+    validates :address
+    validates :order_id
+    validates :user_id
+    validates :item_id
+    validates :zip_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number,format: {with: /\A[0-9]{3}-[0-9]{4}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+    validates :prefecture_id,numericality: { other_than: 1 } 
+
 end
--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -5,10 +5,6 @@ class Shipping < ApplicationRecord
 
   belongs_to :order
 
-  with_options presence:true  do
-    validates :zip_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :city
-    validates :address
-    validates :phone_number,format: {with: /\A[0-9]{3}-[0-9]{4}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id,numericality: { other_than: 1 } 
+  
 end
+
