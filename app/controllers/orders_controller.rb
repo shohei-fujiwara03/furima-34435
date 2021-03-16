23a4a4f6f74f221529ad0a069d6b46daec77f6d4
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-03-16 15:21:34 +0900

    テーブルに情報が保存されるよう修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,18 +8,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item =Item.find(params[:item_id])
     @order_shipping = OrderShipping.new(order_params)
     if @order_shipping.valid?
       @order_shipping.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_shipping).permit(:zip_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id,item_id:current_item.id)
+    params.require(:order_shipping).permit(:zip_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id:current_user.id,item_id:@item.id)
   end
 end
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -5,7 +5,6 @@ class OrderShipping
   with_options presence:true  do
     validates :city
     validates :address
-    validates :order_id
     validates :user_id
     validates :item_id
     validates :zip_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -13,8 +12,8 @@ class OrderShipping
   end
     validates :prefecture_id,numericality: { other_than: 1 } 
 
-    def index
-      order = Order.create(user_id:user_id, item_id: item_id)
-      Shipping.create(zip_code: zip_code, prefecture_id:prefecture_id, address:address,building_name:building_name,phone_number:phone_number,order_id:order.id )
+    def save
+      order = Order.create(user_id: user_id, item_id: item_id)
+      Shipping.create(zip_code: zip_code, prefecture_id:prefecture_id,city:city, address:address,building_name:building_name,phone_number:phone_number,order_id:order.id )
     end
 end
