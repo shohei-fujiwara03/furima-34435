a404cb35aa76b8e31a968b44cfe8d5b0ca136d09
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-03-17 19:49:20 +0900

    レビューによる修正(4)

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,6 +1,6 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :zip_code,:prefecture_id,:city,:address,:building_name,:phone_number,:order_id,:user_id,:item_id,:token
+  attr_accessor :zip_code,:prefecture_id,:city,:address,:building_name,:phone_number,:order_id,:item_id,:token
 
   with_options presence:true  do
     validates :city
