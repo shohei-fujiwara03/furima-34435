e102e55413e296d64ffc97bbb907feb86d4bc491
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-03-24 15:46:23 +0900

    ファイル選択ボタンの位置調整

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -6,20 +6,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const createImageHTML = (blob) => {
     // 画像を表示するためのdivを生成
     const imageElement = document.createElement('div')
-    imageElement.style.display = "block";
     imageElement.setAttribute('class', "image-element")
     let imageElementNum = document.querySelectorAll('.image-element').length
 
     // 表示する画像を生成
     const blobImage = document.createElement('img')
     blobImage.setAttribute('src', blob)
+    blobImage.style.margin = '50px 200px';
 
     // ファイル選択ボタンを生成
     const inputHTML = document.createElement('input')
     inputHTML.setAttribute('id', `item_image_${imageElementNum}`)
     inputHTML.setAttribute('name', 'item[images][]')
     inputHTML.setAttribute('type', 'file')
-    inputHTML.style.padding = '10px';
+    inputHTML.style.margin = '20px 0 20px 10px';
+    inputHTML.style.style = 'block';
 
     // 生成したHTMLの要素をブラウザに表示させる
     imageElement.appendChild(blobImage)
