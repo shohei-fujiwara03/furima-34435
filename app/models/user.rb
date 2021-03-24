877a32ea8b37b59edb2daa7669489987b5197058
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-03-24 17:44:55 +0900

    ユーザー登録時のエラーメッセージの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,17 @@ class User < ApplicationRecord
          validates :nickname
          validates :birth_date
 
-         with_options format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/,message:"must be full-pitch character"} do
+         with_options format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/,message:"は全角でなければ登録できません"} do
          validates :family_name
          validates :first_name
          end
 
-         with_options format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,message:"must be full-width katakana"} do
+         with_options format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,message:"は全角カタカナでないと登録できません"} do
          validates :family_name_kana
          validates :first_name_kana
          end
          
-         validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/,message:"include both letters and numbers"}
+         validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/,message:"は英数混合でなければ登録できません"}
         end
 
 
